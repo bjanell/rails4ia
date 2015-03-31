ce10bb1cbb222dfa116aad32c4b2dee11934993c
Ben Anello <ben@anello.us.com>
2015-03-31 11:20:01 -0600

    Uploaded assets are now served through the AssetsController for extra security.

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
+
+  resources :assets, only: [:show], path: :files
 end
