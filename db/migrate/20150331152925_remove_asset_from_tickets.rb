e1f3038781387a4cf64f4e682b9e5774e469f715
Ben Anello <ben@anello.us.com>
2015-04-08 10:02:18 -0600

    Remove the extra asset files from the old code.

--- db/migrate/20150331152925_remove_asset_from_tickets.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAssetFromTickets < ActiveRecord::Migration
-  def change
-    remove_column :tickets, :asset, :string
-  end
-end
--- spec/factories/asset_factory.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :asset do
-    transient do
-      file 'spec/fixtures/speed.txt'
-    end
-
-    asset { File.open file }
-  end
-end
