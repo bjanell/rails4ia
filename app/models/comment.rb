fd86e1c8efc6ba584af2df0f665ae109ccf98e7f
Ben Anello <ben@anello.us.com>
2015-04-10 14:03:03 -0600

    Users can add tags when adding a comment.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,14 +4,25 @@ class Comment < ActiveRecord::Base
   belongs_to :state
   belongs_to :previous_state, class_name: 'State'
 
+  attr_accessor :tag_names
+
   before_create :set_previous_state
   after_create :set_ticket_state
+  after_create :associate_tags_with_tickets
 
   validates :text, presence: true
 
   delegate :project, to: :ticket
 
   scope :persisted, lambda { where.not(id: nil) }
+
+  def associate_tags_with_tickets
+    if tag_names
+      tag_names.split.each do |name|
+        ticket.tags << Tag.find_or_create_by(name: name)
+      end
+    end
+  end
 
   private
 
